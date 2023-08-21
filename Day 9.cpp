8e941ffc16771c059972297dfb9ed965e007ca45
Adarsh1Rao <90914622+Adarsh1Rao@users.noreply.github.com>
2023-08-22 02:17:17 +0530

    Day 9

---
+++ Day 9.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+#include <utility>
+
+pair<char, int> averageMarks(char firstLetterOfName, int m1, int m2, int m3) {
+    // Write your code here.
+
+    int sum=m1+m2+m3;
+    int ans=sum/3;
+
+    pair<char,int>p={ firstLetterOfName,ans};
+    return p;
+    
+}
